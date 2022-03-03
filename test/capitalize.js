b8d14c3edfcc8d3877808fbc825d3d989256473f
Zeal22 <fevral12@mail.ru>
2022-03-03 23:28:17 +0500

    naming

--- test/capitalize.js
+++ test/capitalize.js
@@ -1,5 +1,5 @@
-const capitalize = (text) => {
+function capitalize(text) {
 	const firstChar = text[0].toUpperCase();
 	const restSubstring = text.slice(1);
 	return `${firstChar}${restSubstring}`;
-};
+}
