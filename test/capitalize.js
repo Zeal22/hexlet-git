d26d9a8461a8f6edcfbb8beacc3f1637afcd2894
Zeal22 <fevral12@mail.ru>
2022-03-03 23:39:46 +0500

    some text

--- test/capitalize.js
+++ test/capitalize.js
@@ -2,4 +2,5 @@ function capitalize(text) {
 	const firstChar = text[0].toUpperCase();
 	const restSubstring = text.slice(1);
 	return `${firstChar}${restSubstring}`;
-}
+};
+export default capitalize;
