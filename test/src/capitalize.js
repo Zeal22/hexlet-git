de41b55555980e45dbb0a7025e8b1b9850ef931c
Zeal22 <fevral12@mail.ru>
2022-03-04 12:33:11 +0500

    fix capitalize

--- test/src/capitalize.js
+++ test/src/capitalize.js
@@ -1,6 +1,4 @@
-function capitalize(text) {
-	const firstChar = text[0].toUpperCase();
-	const restSubstring = text.slice(1);
-	return `${firstChar}${restSubstring}`;
-};
-export default capitalize;
+export const capitalize = (text) => {
+	const [firstSymbol = '', ...restSymbols] = text;
+	return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
+  };
